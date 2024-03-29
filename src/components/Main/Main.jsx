4e8241a2d9e1b55cbbc61e3530bfeee24f897192
Naim <mdnaim01303202218@gmail.com>
2024-03-30 03:10:48 +0600

    UI part Done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -33,8 +33,14 @@ const Real = () => {
           </div>
           <div className="main-bottom">
             <div className="search-box">
-              
+              <input type="text" placeholder='Enter a prompt here'/>
+              <div>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img src={assets.send_icon} alt="" />
+              </div>
             </div>
+            
           </div>
         </div>
     </div>
